aaacc0fea958c058d31c2c59747624d2795f6e6f
feyzaozcann <77546939+feyzaozcann@users.noreply.github.com>
2021-02-13 17:59:45 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,24 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryProductDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("CarId ="+car.Id);
+                Console.WriteLine("Brand ="+car.BrandId);
+                Console.WriteLine("Daily Price ="+car.DailyPrice);
+                Console.WriteLine("Description ="+car.Description);
+                Console.WriteLine("Model Year ="+car.ModelYear);
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,53 @@
+﻿
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryProductDal()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1, BrandId=2 , DailyPrice=200 , ModelYear="2020" , Description="For 2 people" },
+                new Car{Id=2, BrandId=3 , DailyPrice=40 , ModelYear="2016" , Description="For 1 person" },
+                new Car{Id=3, BrandId=1 , DailyPrice=220 , ModelYear="2021" , Description="For 2 people" },
+                new Car{Id=4, BrandId=2 , DailyPrice=170 , ModelYear="2019" , Description="For 2 people" }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => car.Id == car.Id);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => car.Id == car.Id);
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+
+        List<Car> ICarDal.GetById(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+    }
+}
